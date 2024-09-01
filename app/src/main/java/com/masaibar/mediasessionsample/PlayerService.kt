6d6323606dfe93f6801db0e257560459e90c6873
masaibar <masaibar.dev@gmail.com>
2024-09-01 20:09:12 +0900

    PlayerServiceの追加

---
+++ app/src/main/java/com/masaibar/mediasessionsample/PlayerService.kt
@@ -0,0 +1,34 @@
+package com.masaibar.mediasessionsample
+
+import android.content.Intent
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSessionService
+
+class PlayerService : MediaSessionService() {
+    private var mediaSession: MediaSession? = null
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? =
+        mediaSession
+
+    override fun onCreate() {
+        super.onCreate()
+        val player = ExoPlayer.Builder(this).build()
+        mediaSession = MediaSession.Builder(this, player).build()
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+        // TODO: Playerの細かな状態でハンドリングする
+        stopSelf()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaSession?.run {
+            player.release()
+            release()
+            mediaSession = null
+        }
+    }
+}
